3ecc48147ff4e0d1783b3acd5dd52cdcb4a61a22
Patrick O'Connor <pattyo20@optonline.net>
2023-11-20 23:00:45 -0500

    last one tonight

--- app.ts
+++ app.ts
@@ -141,17 +141,6 @@ app.post("/auth/login", async (req: Request, res: Response) => {
       });
     }
 
-    // Set CORS headers
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-
     return res.status(200).json({
       status: 200,
       success: true,
--- dist/app.js
+++ dist/app.js
@@ -128,10 +128,6 @@ app.post("/auth/login", (req, res) => __awaiter(void 0, void 0, void 0, function
                 message: "Incorrect password",
             });
         }
-        // Set CORS headers
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         return res.status(200).json({
             status: 200,
             success: true,
